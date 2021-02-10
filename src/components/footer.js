d7a9de022cd526919247cab23bdf9f64815db568
Johan Félisaz <johan.felisaz@protonmail.com>
2021-02-10 20:34:18 +0100

    added latex support

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import * as React from "react"
 export default function Footer() {
     return <footer>
 	       <p>
-		   &copy; Johan Félisaz 2021.
+		   &copy; 2021, Johan Félisaz. 
 		   Generated using <a href="https://www.gatsbyjs.com/">Gatsby</a>
 	       </p>
 	   </footer>
--- src/templates/note-template.js
+++ src/templates/note-template.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { Helmet } from "react-helmet"
 
 import Layout from "../components/layout"
 
@@ -7,6 +8,10 @@ export default function NoteTemplate({ data }) {
     const title = note.metadata.title
     return (
 	<Layout>
+	    <Helmet>
+		<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+		<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+	    </Helmet>
 	    <h3>{title}</h3>
 	    <div dangerouslySetInnerHTML={{__html: note.html}} />
 	</Layout>
