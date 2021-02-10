54c4058ab8cc6b0a45861e26f420bf358b301025
Johan Félisaz <johan.felisaz@protonmail.com>
2021-02-10 17:00:59 +0100

    project + css license

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,6 @@ import { Link } from "gatsby"
 
 function MenuLink({ to, children }) {
     return <li style={{ "list-style-type": "none",
-			
 		      }}>
 	       <Link to={to}>{children}</Link>
 	   </li>
@@ -16,6 +15,7 @@ export default function Header() {
 		   <ul style={{ display: "flex",
 			        "justify-content": "space-around"}}>
 		       <MenuLink to="/">Home</MenuLink>
+		       <MenuLink to="/projects">Projects</MenuLink>
 		       <MenuLink to="/about">About</MenuLink>
 		   </ul>
 	       </nav>
---
+++ src/pages/projects.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+
+export default function Projects() {
+    return (
+	<Layout>
+	    <h3>My projects</h3>
+	</Layout>
+    );
+}
