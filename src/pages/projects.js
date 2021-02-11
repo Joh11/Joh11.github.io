f64c9ed8346f24f145f4ed4302ad507926882ab7
Johan Félisaz <johan.felisaz@protonmail.com>
2021-02-11 17:46:48 +0100

    basic projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,22 @@
 import * as React from "react"
 
 import Layout from "../components/layout"
+import data from "../projects/projects.json"
+
+function Project({ project }) {
+    const { url, title } = project
+    return <li>
+	       <a href={ url }>{ title }</a>
+	   </li>
+}
 
 export default function Projects() {
     return (
 	<Layout>
 	    <h3>My projects</h3>
+	    <ul>
+		{ data.projects.map(project => <Project project={project}/>) }
+	    </ul>
 	</Layout>
     );
 }
