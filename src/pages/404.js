70d05783ba3f4accb9c7365ef61eead36aa829f3
Johan Félisaz <johan.felisaz@protonmail.com>
2021-02-11 20:58:29 +0100

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,12 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+import Layout from "../components/layout"
 
-const paragraphStyles = {
-  marginBottom: 48,
+export default function NotFoundPage() {
+    return <Layout>
+	       <h2>404</h2>
+	       <p>We could not find what you were looking for. </p>
+	       <Link to="/">Go home</Link>
+	   </Layout>
 }
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
-const NotFoundPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
-
-export default NotFoundPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,10 @@ import Layout from "../components/layout"
 export default function Index() {
     return (
 	<Layout>
+	    <p>
+		Hi ! My name is Johan Félisaz, I am a Physics Master student at
+		EPFL, Lausanne, Switzerland. 
+	    </p>
 	</Layout>
     )
 }
